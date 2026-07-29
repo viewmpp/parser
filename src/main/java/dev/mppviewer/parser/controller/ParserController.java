de65fc12046b645bc30aa241ab50a0e5fed64dab
dzenthai <dzenthai@proton.me>
2026-07-29 22:16:21 +0500

    refactor: controller parse method

--- src/main/java/dev/mppviewer/parser/controller/ParserController.java
+++ src/main/java/dev/mppviewer/parser/controller/ParserController.java
@@ -1,16 +1,26 @@
 package dev.mppviewer.parser.controller;
 
+import dev.mppviewer.parser.service.ProjectParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
 
 
 @RestController
 public class ParserController {
 
-    @GetMapping("/parse")
-    public ResponseEntity<?> parse() {
-        return new ResponseEntity<>("data", HttpStatus.OK);
+    private final ProjectParser projectParser;
+
+    public ParserController(ProjectParser projectParser) {
+        this.projectParser = projectParser;
+    }
+
+    @PostMapping(value = "/parse", consumes = APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<?> parse(@RequestBody byte[] body) {
+        return new ResponseEntity<>(projectParser.parse(body), HttpStatus.OK);
     }
 }
