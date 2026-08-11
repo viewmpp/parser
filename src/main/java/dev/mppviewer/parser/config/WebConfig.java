274ba5eecdaa769fe4aa43771501382f7c62e8ca
dzenthai <dzenthai@proton.me>
2026-08-11 23:49:20 +0500

    feat: docker compose

--- src/main/java/dev/mppviewer/parser/config/WebConfig.java
+++ src/main/java/dev/mppviewer/parser/config/WebConfig.java
@@ -16,6 +16,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(bodySizeInterceptor);
+        registry.addInterceptor(bodySizeInterceptor).addPathPatterns("/parse");
     }
 }
